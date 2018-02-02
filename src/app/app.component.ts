6215b7389165e9422bdf1c14f15d0e9ff7f073c4
unknown <mahmoudmagdy1919@hotmail.com>
2018-02-02 12:22:41 -0800

    test commint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
+import { HomePage , TakePicPage , SearchMedicalPage } from '../pages/pages';
 
 @Component({
   templateUrl: 'app.html'
@@ -31,9 +31,10 @@ export class MyApp {
     });
   }
 
-  openPage(page) {
-    // Reset the content nav to have just this page
-    // we wouldn't want the back button to show in this scenario
-    this.nav.setRoot(page.component);
+  OpenTakePicPage(){
+      this.nav.push(TakePicPage);
+  }
+  OpenSearchMedicalPage(){
+    this.nav.push(SearchMedicalPage);
   }
 }
